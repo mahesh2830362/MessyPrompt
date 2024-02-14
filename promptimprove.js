ca2bf5d297c8dd2aa9ed6976769044f376966f1b
mahesh2830362 <130383828+mahesh2830362@users.noreply.github.com>
2024-02-14 08:45:50 -0500

    Create promptimprove.js

---
+++ promptimprove.js
@@ -0,0 +1,4 @@
+document.getElementById('improve-prompt').addEventListener('click', () => {
+  let messyPrompt = document.getElementById('messy-input').value;
+  // Code to improve the messy prompt
+});
